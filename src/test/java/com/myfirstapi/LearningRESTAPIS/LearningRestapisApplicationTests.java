8778c785eb82447ee7089f351edff42562ea58f4
Rohan <rohanotc1@gmail.com>
2026-03-01 13:14:27 +0530

    Initial commit

---
+++ src/test/java/com/myfirstapi/LearningRESTAPIS/LearningRestapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirstapi.LearningRESTAPIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRestapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
